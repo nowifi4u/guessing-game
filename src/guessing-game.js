6fe793b3f4e415c4fbde614a353930a19828088a
NoWifi4U <max.shamber@gmail.com>
2019-10-02 22:51:34 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
+    static getMiddle(a, b){
+        if ((a + b) % 2 != 0) return (a + b + 1) / 2;
+        return (a + b) / 2;
+    }
+
+    getMiddle(){
+        return GuessingGame.getMiddle(this.min, this.max);
     }
 
     guess() {
-
+        return this.getMiddle();
     }
 
     lower() {
-
+        this.max = this.getMiddle();
     }
 
     greater() {
-
+        this.min = this.getMiddle();
     }
 }
 
